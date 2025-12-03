d1d66b5749c65cd7001bd5d6109abafbee99cb7a
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-12-03 10:39:21 +0000

    Add exercise

--- debugging.py
+++ debugging.py
@@ -7,44 +7,11 @@ from lib.most_often import MostOften
 
 
 mo = MostOften(['a', 'b'])
+mo.add_new('a')
+mo.add_new('b')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# print("========")
+# print(mo.get_most_often())
 
 
 print(get_character_count('I believe I can fly'))
